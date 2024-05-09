450129351efed4ac50bc20dee11290ae6c82c108
elskater98 <elskater98@gmail.com>
2024-05-10 00:16:09 +0200

    Integration Minio and Spark: DONE

---
+++ README.md
@@ -0,0 +1,34 @@
+# MINIO
+
+Firstly, you need to create a ACCESS KEY to work with MINIO via API. So, create it and save the credentials in secure
+place.
+
+Secondly, to test the connection you can use the below script
+
+    # pip install minio
+
+    from minio import Minio
+
+    client = Minio("localhost:9000",
+        access_key="<ACCESS_KEY>",
+        secret_key="<SECRET_KEY>",
+        secure=False ## Set the value True if you use HTTPS.
+    )
+
+    client.list_buckets()
+
+Upload `customer_booking.csv` file to bucket project.
+
+# Environment Variables
+
+    MINIO_ENDPOINT=http://minio:9000 # container name, ip container or host
+    AWS_ACCESS_KEY_ID=Yrg1PWyaB1x1DcaR5wHx # example KEY ID
+    AWS_SECRET_ACCESS_KEY=6tJdw7vgxBoKKTQ7ZXpX6ukomcgugzzR4xJQ8Ejo # example ACCESS KEY
+
+# Spark
+Add env. AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, MINIO_ENDPOINT variables to spark environment.
+
+
+To execute script use:
+
+    spark-submit scripts/main.py
--- data/spark/scripts/main.py
+++ data/spark/scripts/main.py
@@ -1,9 +1,49 @@
+import logging
+import os
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("MinioSparkJob")
+
+
+# REFERENCES
+# https://stackoverflow.com/questions/75770134/bitnami-spark-installation-of-jars-and-connection-to-bitnami-minio
+# https://stackoverflow.com/questions/69680552/minio-spark-integration
+# https://blog.min.io/spark-minio-kubernetes/
+
+def load_config(spark_context: SparkContext):
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.access.key",
+                                                 os.getenv("AWS_ACCESS_KEY_ID"),
+                                                 )
+
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.secret.key",
+                                                 os.getenv("AWS_SECRET_ACCESS_KEY"),
+                                                 )
+
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.endpoint",
+                                                 os.getenv("MINIO_ENDPOINT"))  # container name or ip / host
+
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.attempts.maximum", "1")
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.connection.establish.timeout", "5000")
+    spark_context._jsc.hadoopConfiguration().set("fs.s3a.connection.timeout", "10000")
+
+
 if __name__ == '__main__':
-    # https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/dataframe.html#
-    # https://medium.com/@mehmood9501/using-apache-spark-docker-containers-to-run-pyspark-programs-using-spark-submit-afd6da480e0f
-    spark = SparkSession.builder.appName("Spark-Test").getOrCreate()
-    df = spark.read.csv('datasets/customer_booking.csv', header=True, inferSchema=True, sep=',')
-    df = df.filter((df.booking_complete == 0) & (df.sales_channel == 'Internet'))
-    df.groupby('booking_origin')
+    spark = SparkSession.builder.appName("MinioSparkJob").getOrCreate()
+
+    load_config(spark.sparkContext)
+
+    df = spark.read.option("header", "true").csv(f"s3a://project/customer_booking.csv")
+
+    df = df.filter(
+        (df.booking_complete == 0) & (df.sales_channel == 'Internet') & (df.booking_origin == 'South Korea'))
+
+    logger.info("Filter by sales channel == Internet & booking_complete == 0 & booking_origin == South Korea")
+
+    df.write.format("csv").option("header", "true").mode('overwrite').save('s3a://project/customer_booking_OUTPUT')
